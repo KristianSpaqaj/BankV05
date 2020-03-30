e6b38535fdcde43aece6a2074f4020fa9d827f4c
KristianSpaqaj <kris49j7@edu.easj.dk>
2020-03-30 13:26:56 +0200

    Initial commit

---
+++ InsertCodeHere.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace BankV05
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            BankAccount myAccount = new BankAccount("Kristian Spaqaj");
+
+            myAccount.Deposit(2000);
+            Console.WriteLine($"Account balance for {myAccount.accountHolderName} is : {myAccount.Balance}");
+
+            myAccount.Withdraw(1500);
+            Console.WriteLine($"Account balance for {myAccount.accountHolderName} is : {myAccount.Balance}");
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
